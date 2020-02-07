7c07614e9137afd99cec62f48ad03cd8f8ebe4c6
d.epe <d.epe@gambio.de>
2020-02-07 08:05:09 +0100

    finished the class implementation

--- classes/Variable.php
+++ classes/Variable.php
@@ -1,42 +1,85 @@
 <?php
 
+/* --------------------------------------------------------------
+  GProtectorFilter.php 2020-02-07
+  Gambio GmbH
+  http://www.gambio.de
+  Copyright (c) 2020 Gambio GmbH
+  Released under the GNU General Public License (Version 2)
+  [http://www.gnu.org/licenses/gpl-2.0.html]
+  --------------------------------------------------------------*/
+
 namespace GProtector;
+
+use \InvalidArgumentException;
+use \UnexpectedValueException;
 
 class Variable
 {
-  private $type;
-  private $property = [];
+    /**
+     * @var String $type
+     */
+    private $type;
+    
+    /**
+     * @var array $property
+     */
+    private $property = [];
     
     
     public function __construct($type, $property)
     {
-    
-  }
-    
-    private function validateType($type)
-    {
-        if ($type === null || (is_string($type)) === false) {
-            throw new InvalidArgumentException('Invalid $type \'' . $type . '\'');
-        }
-    }
-    
-    private function validateProperty($property)
-    {
-        if ($property === null || (is_array($property)) === false) {
-            throw new InvalidArgumentException('Invalid $property \'' . $property . '\'');
-        }
+        $this->validateType($type);
+        $this->validateProperty($property);
         
-        foreach ($property as $propertyValue){
-          
-            if ($propertyValue === null ||(is_string($propertyValue ))=== false){
-              throw  new \UnexpectedValueException('Invalid $propertyValue \'' . $propertyValue . '\'');
-            }
-    }
+        $this->type     = $type;
+        $this->property = $property;
     }
     
     
     /**
-     * @return mixed
+     * Validates type
+     *
+     * @param mixed $type to be validated
+     *
+     * @throws InvalidArgumentException if type is null or not a string
+     */
+    private function validateType($type)
+    {
+        if ($type === null || (is_string($type)) === false) {
+            throw new InvalidArgumentException('Invalid $type');
+        }
+    }
+    
+    
+    /**
+     * Validates property
+     *
+     * @param mixed $property to be validated
+     *
+     * @throws InvalidArgumentException, UnexpectedValueException
+     *   if property is null or not an array or if property value is not a string or null
+     *
+     */
+    private function validateProperty($property)
+    {
+        if ($property === null || (is_array($property)) === false) {
+            throw new InvalidArgumentException('Invalid $property');
+        }
+        
+        foreach ($property as $propertyValue) {
+            
+            if ($propertyValue === null || (is_string($propertyValue)) === false) {
+                throw  new UnexpectedValueException('Invalid $propertyValue');
+            }
+        }
+    }
+    
+    
+    /**
+     * Getter for type
+     *
+     * @return String
      */
     public function type()
     {
@@ -45,6 +88,8 @@ class Variable
     
     
     /**
+     * Getter for property
+     *
      * @return array
      */
     public function property()
