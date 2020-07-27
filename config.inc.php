cf130a7d2bdd315eb45659039b149712788b01fd
Timm Ortloff <t.ortloff@gambio.de>
2020-07-27 13:09:15 +0200

    Renamed Cached FilterRule Filename

--- config.inc.php
+++ config.inc.php
@@ -25,4 +25,4 @@ define('GAMBIO_PROTECTOR_LOG_MAX_FILESIZE', 1); // megabytes
 define('GAMBIO_PROTECTOR_REMOTE_FILTERRULES_URL', 'http://ton.dev.gambio-server.net/standard.json');
 define('GAMBIO_PROTECTOR_LOCAL_FILERULES_DIR', dirname(__FILE__) . '/filter/');
 define('GAMBIO_PROTECTOR_LOCAL_FILERULES_FILENAME', 'standard.json');
-define('GAMBIO_PROTECTOR_CACHE_FILERULES_FILENAME', 'cache_standard.json');
+define('GAMBIO_PROTECTOR_CACHE_FILERULES_FILENAME', 'standard.json');
