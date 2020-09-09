becc4051c18c23bb8eb9581a874de7b2ada44dda
Timm Ortloff <t.ortloff@gambio.de>
2020-09-09 10:26:29 +0200

    Performance Optimization

--- config.inc.php
+++ config.inc.php
@@ -8,22 +8,22 @@
   [http://www.gnu.org/licenses/gpl-2.0.html]
   --------------------------------------------------------------*/
 
-define('GAMBIO_PROTECTOR_DIR', dirname(__FILE__) . '/');
-define('GAMBIO_PROTECTOR_CLASSES_DIR', dirname(__FILE__) . '/classes/');
-define('GAMBIO_PROTECTOR_CONNECTORS_DIR', dirname(__FILE__) . '/classes/connectors/');
-define('GAMBIO_PROTECTOR_FUNCTIONS_DIR', dirname(__FILE__) . '/functions/');
-define('GAMBIO_PROTECTOR_FILTER_DIR', dirname(__FILE__) . '/filter/');
-define('GAMBIO_PROTECTOR_LOG_DIR', dirname(dirname(__FILE__)) . '/logfiles/');
-define('GAMBIO_PROTECTOR_TOKEN_DIR', dirname(dirname(__FILE__)) . '/media/');
-define('GAMBIO_PROTECTOR_CACHE_DIR', dirname(__FILE__) . '/cache/');
-define('GAMBIO_PROTECTOR_BASE_DIR', dirname(dirname(__FILE__)) . '/');
+define('GAMBIO_PROTECTOR_DIR', __DIR__ . '/');
+define('GAMBIO_PROTECTOR_CLASSES_DIR', __DIR__ . '/classes/');
+define('GAMBIO_PROTECTOR_CONNECTORS_DIR', __DIR__ . '/classes/connectors/');
+define('GAMBIO_PROTECTOR_FUNCTIONS_DIR', __DIR__ . '/functions/');
+define('GAMBIO_PROTECTOR_FILTER_DIR', __DIR__ . '/filter/');
+define('GAMBIO_PROTECTOR_LOG_DIR', __DIR__ . '/../logfiles/');
+define('GAMBIO_PROTECTOR_TOKEN_DIR', __DIR__ . '/../media/');
+define('GAMBIO_PROTECTOR_CACHE_DIR', __DIR__ . '/cache/');
+define('GAMBIO_PROTECTOR_BASE_DIR', __DIR__ . '/../');
 define('GAMBIO_PROTECTOR_FUNCTION_PREFIX', 'gprotector_');
 define('GAMBIO_PROTECTOR_TOKEN_FILE_PREFIX', 'gprotector_token_');
 define('GAMBIO_PROTECTOR_FILE_PATTERN', '*.json');
 define('GAMBIO_PROTECTOR_GZIP_LOG', true);
 define('GAMBIO_PROTECTOR_LOG_MAX_FILESIZE', 1); // megabytes
 define('GAMBIO_PROTECTOR_REMOTE_FILTERRULES_URL', 'https://protect.gambio-server.net/standard.json');
-define('GAMBIO_PROTECTOR_LOCAL_FILERULES_DIR', dirname(__FILE__) . '/filter/');
+define('GAMBIO_PROTECTOR_LOCAL_FILERULES_DIR', __DIR__ . '/filter/');
 define('GAMBIO_PROTECTOR_LOCAL_FILERULES_FILENAME', 'standard.json');
 define('GAMBIO_PROTECTOR_CACHE_FILERULES_FILENAME', 'standard.json');
 define('GAMBIO_PROTECTOR_CACHE_RENEW_INTERVAL', 8*60*60); // In seconds
--- start.inc.php
+++ start.inc.php
@@ -12,38 +12,30 @@ use GProtector\FilterCache;
 use GProtector\FilterReader;
 use GProtector\GProtector;
 
-if (defined('E_DEPRECATED')) {
-    error_reporting(
-        E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_CORE_ERROR & ~E_CORE_WARNING
-    );
-} else {
-    error_reporting(
-        E_ALL & ~E_NOTICE & ~E_STRICT & ~E_CORE_ERROR & ~E_CORE_WARNING
-    );
-}
+error_reporting(
+    E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_CORE_ERROR & ~E_CORE_WARNING
+);
 
 if (!class_exists('\Composer\Autoload\ClassLoader', false)) {
     require_once __DIR__ . '/../vendor/autoload.php';
 }
 
-require_once(dirname(__FILE__) . '/config.inc.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/GProtectorLogConnectorInterface.inc.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/GProtector.inc.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/Filter.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/FilterCache.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/FilterCollection.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/FilterReader.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/Key.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/Method.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/ScriptName.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/ScriptNameCollection.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/Severity.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/Variable.php');
-require_once(GAMBIO_PROTECTOR_CLASSES_DIR . '/VariableCollection.php');
+require_once __DIR__ . '/config.inc.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/GProtectorLogConnectorInterface.inc.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/GProtector.inc.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/Filter.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/FilterCache.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/FilterCollection.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/FilterReader.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/Key.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/Method.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/ScriptName.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/ScriptNameCollection.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/Severity.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/Variable.php';
+require_once GAMBIO_PROTECTOR_CLASSES_DIR . '/VariableCollection.php';
 
-$reader = new FilterReader();
-$cache = new FilterCache();
 
-$gprotector = new GProtector($reader, $cache);
+$gprotector = new GProtector(new FilterReader(), new FilterCache());
 $gprotector->start();
 unset($gprotector);
